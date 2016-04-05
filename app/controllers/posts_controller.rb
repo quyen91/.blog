9bd057bf89a2f9aec7c97c58ee884954f99a942a
quyen91 <chibedethuong91@gmail.com>
2016-04-06 01:54:58 +0700

    fix tag add and display

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,7 +29,7 @@ class PostsController < ApplicationController
   end
   private
   def post_params
-    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:title, :content, :tag_names)
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ActiveRecord::Base
+	has_many :taggings,  dependent: :destroy
+	has_many :tags, through: :taggings
+	validates_presence_of :title, :content
+
+	attr_accessor :tag_names
+	after_save :assign_tags
+
+	private
+	def assign_tags
+		if @tag_names
+			self.tags = @tag_names.split(",").map do |name|
+				# Tag.find_or_create_by_name(name)
+				Tag.where(name: name.strip).first_or_create!
+			end
+		end
+	end
+	def list_tags
+		self.tags.map(&:name).join(", ")
+	end
 end
