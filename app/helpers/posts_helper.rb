4b7c757fc356f8e73952e28e8981dd365b4ffd3a
quyen91 <chibedethuong91@gmail.com>
2016-04-10 12:36:05 +0700

    fix post item css

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -6,4 +6,7 @@ module PostsHelper
 	# 		yield(tag, classes[index.round])
 	# 	end
 	# end
+	def tag_links(tags)
+  	tags.split(",").map{|tag| link_to tag.strip, tag_path(tag.strip) }.join(" ") 
+	end
 end
