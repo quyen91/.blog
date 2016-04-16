218e0801041eb64cc7966de99322ae59fc71fd73
quyen91 <chibedethuong91@gmail.com>
2016-04-16 22:19:40 +0700

    config javascript

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
-	alert("dd");
-	$("#new_status").submit(function(){
-		// $("#new_status").val('');
-		event.preventDefault();
-	});
+// $(document).ready(function(){
+// 	alert("dd");
+// 	$("#new_status").submit(function(){
+// 		// $("#new_status").val('');
+// 		event.preventDefault();
+// 	});
 
-});
+// });
